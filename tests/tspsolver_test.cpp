58ecc94e5e744bb74fcfd1798080a0b11f9cf0ac
njanirudh <anijaya9@gmail.com>
2021-06-10 14:16:58 +0200

    Added tests

--- tests/tspsolver_test.cpp
+++ tests/tspsolver_test.cpp
@@ -1,1 +1,17 @@
 #include "catch.hpp"
+
+
+TEST_CASE("Matrix constructor", "[constructor]")
+{
+  SECTION("Identity"){
+   REQUIRE(1 == 1);
+  }
+
+  SECTION("Same Values"){
+  REQUIRE(1 == 1);
+  }
+
+  SECTION("Initializer List"){
+     REQUIRE(1 == 1);
+  }
+}
